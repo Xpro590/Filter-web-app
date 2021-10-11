6261dc442974cad69cb9cedba2690d35096b188f
Xpro590 <92288032+Xpro590@users.noreply.github.com>
2021-10-11 17:09:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload()
+{
+
+}
+
+function take_snapshot()
+{
+  save('person.png') ;
+}
+
+function filter_tint()
+{
+  var tint_color = document.getElementById("color_name").value ;
+}
